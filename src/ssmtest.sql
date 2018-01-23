cbd3910d5e63300470a80e993e7bcde0d60b46e6
yaozhiyang <1131350876@qq.com>
2018-01-23 10:55:23 +0800

    sql文件

---
+++ src/ssmtest.sql
@@ -0,0 +1,58 @@
+/*
+SQLyog Trial v12.2.3 (32 bit)
+MySQL - 5.5.41 : Database - ssmtest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssmtest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ssmtest`;
+
+/*Table structure for table `group` */
+
+DROP TABLE IF EXISTS `group`;
+
+CREATE TABLE `group` (
+  `gid` int(32) NOT NULL,
+  `gname` varchar(32) DEFAULT NULL,
+  `uid` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`gid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `group` */
+
+insert  into `group`(`gid`,`gname`,`uid`) values 
+(1,'王者组',NULL),
+(2,'青铜',NULL);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `uid` int(32) NOT NULL AUTO_INCREMENT COMMENT '用户唯一id',
+  `username` varchar(32) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`uid`,`username`,`password`) values 
+(1,'二狗','123'),
+(2,'小王','123'),
+(20,'老王','123'),
+(21,'老李','123');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
